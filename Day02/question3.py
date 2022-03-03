b81ae84f80333eb02755715335e6d4092847a287
Mayank-Raj-Au48 <maya9708@gmail.com>
2022-03-03 13:34:50 +0530

    cc

---
+++ Day02/question3.py
@@ -0,0 +1,23 @@
+# Longest Palindrome 
+# https://leetcode.com/problems/longest-palindrome/
+import collections
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        odds = 0
+        for k, v in collections.Counter(s).iteritems():
+            odds += v & 1
+        return len(s) - odds + int(odds > 0)
+
+    def longestPalindrome2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        odd = sum(map(lambda x: x & 1, collections.Counter(s).values()))
+        return len(s) - odd + int(odd > 0)
